cec12bed0a36a19ce3f2d9e0811acf23ed95e562
SURFACE\micha <michaelrivera0206@gmail.com>
2024-08-08 13:39:19 +0900

    @Configuration needed to bring image path up

--- src/main/java/com/horsepower/config/WebMvcConfig.java
+++ src/main/java/com/horsepower/config/WebMvcConfig.java
@@ -1,10 +1,12 @@
 package com.horsepower.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.horsepower.common.FileManagerService;
 
+@Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
 	
